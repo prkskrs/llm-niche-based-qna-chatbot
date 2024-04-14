10161ce7a2b189573e3d00aaf1006357590120df
prakash <prakash@sproutsai.com>
2024-04-15 02:26:04 +0530

    store and fetch feedback added

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ import json
 from transformers import pipeline
 import asyncio
 import pymongo
+from fastapi import HTTPException
+from fastapi import Query
+from bson import ObjectId
+from fastapi.responses import JSONResponse
 
 # MongoDB connection details
 MONGODB_URL = "mongodb+srv://prkskrs:1JRRLP0TScJtklaB@cluster0.fncdhdb.mongodb.net/myPrjmtDB?retryWrites=true&w=majority"
@@ -21,13 +25,10 @@ db = client[DB_NAME]
 user_collection = db[USER_COLLECTION]
 
 
-# Download NLTK data for sentence tokenization
 nltk.download('punkt')
 
-# Initialize FastAPI app
 app = FastAPI()
 
-# Allow all origins
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -36,35 +37,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Load pre-trained model
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
-# Initialize summarization pipeline
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
-# Sample data
 with open("academics.json", "r") as file:
-    data = json.load(file)  # Parse JSON data
+    data = json.load(file) 
 
-# Function to encode and compute similarity
 def encode_and_compute_similarity(sentence1, sentence2):
     embeddings = model.encode([sentence1, sentence2])
     return np.dot(embeddings[0], embeddings[1]) / (np.linalg.norm(embeddings[0]) * np.linalg.norm(embeddings[1]))
 
-# Function to merge similar answers and rephrase them
 def merge_and_rephrase(similar_answers):
-    # Merge similar answers
     merged_answer = " ".join(similar_answers)
-    
-    # Tokenize merged answer into sentences
     sentences = sent_tokenize(merged_answer)
-    
-    # Simple rephrasing - capitalize the first letter of every sentence
     rephrased_answer = ". ".join([sentence.capitalize() for sentence in sentences])
-    
     return rephrased_answer
 
-# Function to get similar questions and merge/rephrase answers
 def get_similar_question_and_answer(question):
     similarities = [(q, encode_and_compute_similarity(question, q)) for q in data.keys()]
     max_similarities = sorted(similarities, key=lambda x: x[1], reverse=True)[:2]  # Get top 5 similar questions
@@ -74,27 +63,27 @@ def get_similar_question_and_answer(question):
     return similar_questions, similar_answers
 
 
-# User model for signup
 class UserSignup(BaseModel):
     email: str
     password: str
 
-# User model for login
 class UserLogin(BaseModel):
     email: str
     password: str
 
-# Define request body model
 class Question(BaseModel):
     question: str
 
-# Define endpoint
+class Feedback(BaseModel):
+    question: str
+    answer: str
+    company: str
+    email: str
+
 @app.post("/get_answer/")
 async def get_answer(question: Question):
     similar_questions, similar_answers = get_similar_question_and_answer(question.question)
     merged_and_rephrased_answer = merge_and_rephrase(similar_answers)
-    
-    # Asynchronously summarize merged and rephrased answer
     loop = asyncio.get_event_loop()
     summary_task = loop.run_in_executor(None, summarizer, merged_and_rephrased_answer, 130, 30, False)
     summary = await summary_task
@@ -104,26 +93,52 @@ async def get_answer(question: Question):
 
 @app.post("/signup/")
 async def signup(user: UserSignup):
-    # Check if user already exists
     print(user)
     if user_collection.find_one({"email": user.email}):
         return {"message": "User already exists"}
 
-    # Store user in the database
     user_collection.insert_one({"email": user.email, "password": user.password})
     return {"message": "User created successfully"}
 
-# Login endpoint
 @app.post("/login/")
 async def login(user: UserLogin):
-    # Retrieve user from the database
     stored_user = user_collection.find_one({"email": user.email})
-
-    # Check if user exists and verify password
     if user.password == stored_user["password"]:
         return {"message": "Login successful"}
     else:
         return {"message": "Invalid username or password"}
+
+@app.post("/store_feedback/")
+async def store_feedback(feedback: Feedback):
+    existing_feedback = db.feedback.find_one({"email": feedback.email, "company": feedback.company})
+    if existing_feedback:
+        raise HTTPException(status_code=400, detail="Feedback already exists for this email and company")
+    existing_user = user_collection.find_one({"email": feedback.email})
+    
+    if not existing_user:
+        raise HTTPException(status_code=400, detail="User with this email does not exist")
+    
+    db.feedback.insert_one(feedback.dict())
+    return {"message": "Feedback stored successfully"}
+
+@app.get("/get_feedback/")
+async def get_feedback(company: str = Query(..., description="The name of the company to retrieve feedback for")):
+    feedback_cursor = db.feedback.find({"company": company})
+    feedback_by_email = {}
+    for feedback in feedback_cursor:
+        email_parts = feedback["email"].split("@")
+        email_key = email_parts[0]  # Extract the part before the "@" symbol
+        if email_key in feedback_by_email:
+            feedback_by_email[email_key].append({**feedback, "_id": str(feedback["_id"])})
+        else:
+            feedback_by_email[email_key] = [{**feedback, "_id": str(feedback["_id"])}]
+
+    if not feedback_by_email:
+        return JSONResponse(content={"message": "No feedback available for this company"}, status_code=404)
+
+    return feedback_by_email
+
+
 
 # Run FastAPI app
 if __name__ == "__main__":
